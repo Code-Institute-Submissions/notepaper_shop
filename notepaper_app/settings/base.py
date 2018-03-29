cf09f60cc00781851ad06a4e3736bba521b1667e
Anne Gill <gill01730@gmail.com>
2018-03-29 09:57:05 +0000

    Fixed Stripe, Added Profile and Product Detail Pages

--- notepaper_app/settings/base.py
+++ notepaper_app/settings/base.py
@@ -131,3 +131,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET = os.environ.get("STRIPE_SECRET_KEY")
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
