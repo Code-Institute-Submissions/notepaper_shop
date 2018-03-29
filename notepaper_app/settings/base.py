f00015c3f4ca78030810d19d9a82238deb251380
Anne Gill <gill01730@gmail.com>
2018-03-29 11:05:26 +0000

    Changed the Database to postgres

--- notepaper_app/settings/base.py
+++ notepaper_app/settings/base.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'notepaper_app.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 
 
